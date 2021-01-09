cebe68b07b23db2aec52548b577ddae88013549d
Gantulga-ASUS-Ubuntu <limited.tulgaa@gmail.com>
2021-01-09 16:33:31 +0800

    working depth image

--- sim_ws/src/robot1/scripts/img_disp.py
+++ sim_ws/src/robot1/scripts/img_disp.py
@@ -2,41 +2,30 @@
 
 # from __future__ import print_function
 import numpy as np
-from PIL import Image as ig
-import pygame
+from cv_bridge import CvBridge
+import cv2
 import rospy
 from sensor_msgs.msg import Image
-
-WIDTH = 410
-HEIGHT = 410
 
 
 class ImgDisp:
 
     def __init__(self):
-        pygame.init()
-        self.fps = 60
-        self.win = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.clock = pygame.time.Clock()
+        self.bridge = CvBridge()
+        self.pub = rospy.Publisher('cv2_depth_img_raw', Image, queue_size=1)
 
     def callback1(self, msg):
         print(len(msg.data))
-        print(ord(msg.data))
+        # print(ord(msg.data))
 
     def callback(self, msg):
-        rgb_array = np.zeros((msg.width, msg.height, 3), dtype=np.uint8)
-        for row in range(len(msg.data)/(msg.height*4)-1):
-            for col in range(len(msg.data)/(msg.width*4)-1):
-                # print(ord(msg.data[row*col]))
-                rgb_array[row][col][0] = ord(msg.data[row*col+0])
-                rgb_array[row][col][1] = 0
-                rgb_array[row][col][2] = 0
-                # rgb_array[row][col][2] = ord(msg.data[row*col+3])
-                # print(ord(msg.data[row*col+3]))
-        surf = pygame.surfarray.make_surface(rgb_array)
-        self.win.blit(surf, (5, 5))
-        pygame.display.flip()
-        # self.clock.tick(self.fps)
+        img = self.bridge.imgmsg_to_cv2(msg)
+        image = np.array(img, dtype=np.float32)
+        cv2.normalize(image, image, 0, 255, cv2.NORM_MINMAX)
+        # round to integer values
+        image = np.round(image).astype(np.uint8)
+        cv2.imshow('dst_rt', image)
+        cv2.waitKey(1)
 
     def main(self):
         rospy.init_node('im_disp_node')
